310f2b10fcb0840544625a5971a21f8ff9f08759
Artem <a.khimin@yandex.ru>
2021-05-12 12:38:20 +0300

    Сделал 1 задание

--- GB-01-05/Program.cs
+++ GB-01-05/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Task01
 {
@@ -10,6 +11,13 @@ namespace Task01
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Вставьте с клавиатуры произвольный набор символов и они сохранятся в файле 'text.txt': ");
+
+            string str = Console.ReadLine();
+            string filename = "text.txt";
+            File.WriteAllText(filename, str); // записываем в файл строку
+
+
         }
     }
 }
