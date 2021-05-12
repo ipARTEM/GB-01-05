22f8a57f125df397e2b1a708781e3eac9fe11215
Artem <a.khimin@yandex.ru>
2021-05-12 13:15:37 +0300

    Сделал задание 2

---
+++ Task02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime dateTime = DateTime.Now;
+
+            string str = dateTime.ToString("HH:mm:ss");
+            string filename = "startup.txt";
+            File.AppendAllText(filename, Environment.NewLine);
+            File.AppendAllText(filename, str);
+
+            Console.WriteLine($"В фаил 'startup.txt' дописана строка с текущим временем: {dateTime.ToString("HH:mm:ss")} ");
+
+
+        }
+    }
+}
