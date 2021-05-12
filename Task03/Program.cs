e0a0de9cf37e60e35f247e2cc7e5eb105c35977c
Artem <a.khimin@yandex.ru>
2021-05-12 15:18:21 +0300

    сириализация в бинарный формат

---
+++ Task03/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+namespace Task03
+{
+
+    [Serializable]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите с клавиатуры произвольный набор чисел (0...255), данный набор будет записан в бинарный файл: ");
+
+            string str = "000";
+
+
+            BinaryFormatter binary = new BinaryFormatter();
+            binary.Serialize(new FileStream("s1.bin", FileMode.OpenOrCreate), str);     //сириализация в бинарный формат
+
+
+            
+
+
+            //BinaryFormatter deBinaty = new BinaryFormatter();
+
+            string newStr = (string)binary.Deserialize(new FileStream("s1.bin", FileMode.OpenOrCreate));
+
+
+            Console.WriteLine(newStr);
+
+
+        }
+    }
+}
