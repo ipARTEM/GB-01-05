0c29ab62f4e3ea5e96602938b28b13be72ddadb1
Artem <a.khimin@yandex.ru>
2021-05-12 17:00:41 +0300

    Сделал задание 3

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -15,25 +15,56 @@ namespace Task03
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Введите с клавиатуры произвольный набор чисел (0...255), данный набор будет записан в бинарный файл: ");
-
-            string str = "000";
-
-
+            /*
             BinaryFormatter binary = new BinaryFormatter();
             binary.Serialize(new FileStream("s1.bin", FileMode.OpenOrCreate), str);     //сириализация в бинарный формат
 
-
-            
-
-
-            //BinaryFormatter deBinaty = new BinaryFormatter();
-
-            string newStr = (string)binary.Deserialize(new FileStream("s1.bin", FileMode.OpenOrCreate));
-
+            BinaryFormatter deBinaty = new BinaryFormatter();
+            string newStr = (string)binary.Deserialize(new FileStream("s1.bin", FileMode.Open));  //дисириализация 
 
             Console.WriteLine(newStr);
+            */
+            int text=0;
+            do
+            {
+                
+                Console.WriteLine("Введите с клавиатуры произвольное число (0...255), данное число будет записано в бинарный файл: ");
+                try
+                {
+                    text =Convert.ToInt32(Console.ReadLine());
+
+                    if (0 <= text && text <= 255)
+                    {
+                        break;
+
+                    }
+                }
+                catch (Exception)
+                {
+                    
+                }
+
+            } while (true);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            using (FileStream fs = new FileStream("bin.bin", FileMode.OpenOrCreate))
+            {
+                // сириализация 
+                formatter.Serialize(fs, text);
+
+                Console.WriteLine("Объект сериализован");
+            }
+
+            // десериализация
+            using (FileStream fs = new FileStream("bin.bin", FileMode.Open))
+            {
+                int newText = (int)formatter.Deserialize(fs);
+
+                
+                    Console.WriteLine($"десериализация: {newText} ");
+                
+            }
 
 
         }
