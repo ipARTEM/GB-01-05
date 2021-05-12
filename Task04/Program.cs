8e78d8d104bd7e43d519263230fdcfad8a0ce04a
Artem <a.khimin@yandex.ru>
2021-05-12 19:12:40 +0300

    Сделал 4 задание

---
+++ Task04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = DateTime.Now;
+            string date = dateTime.ToString();
+
+            string line = Path.GetFullPath("task04.txt");
+
+            string lineSave = date+" "+ line;
+
+            string fileName = "task04.txt";
+
+            string fileString = @"D:\Программирование\GeekBrains\C# Разработчик\GB-01-05\Task04\bin\Debug\task04.txt";
+
+            File.AppendAllText(fileName, Environment.NewLine);
+            File.AppendAllText(fileName, lineSave);
+            File.AppendAllText(fileName, Environment.NewLine);
+            File.AppendAllText(fileString, lineSave);
+
+            Console.WriteLine(lineSave);
+
+        }
+    }
+}
